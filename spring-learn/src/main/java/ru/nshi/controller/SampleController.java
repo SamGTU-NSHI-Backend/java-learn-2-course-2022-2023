0608dc85133563f32dcd08833bbfeaea8146ae92
rassafel <rassafel@gmail.com>
2022-11-15 19:54:08 +0400

    Add SampleController.

---
+++ spring-learn/src/main/java/ru/nshi/controller/SampleController.java
@@ -0,0 +1,75 @@
+package ru.nshi.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.bind.annotation.*;
+import ru.nshi.error.MessageNotFoundException;
+import ru.nshi.model.Error;
+import ru.nshi.model.Message;
+import ru.nshi.service.MessageService;
+
+import javax.annotation.PreDestroy;
+
+@RestController
+@RequestMapping(SampleController.MAPPING)
+public class SampleController {
+    public static final String MAPPING = "/sample";
+
+    @Autowired
+    private MessageService service;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+    //    @RequestMapping(path = "/hello", method = {RequestMethod.GET, RequestMethod.HEAD})
+    @GetMapping(path = "/requestParam", produces = {MimeTypeUtils.APPLICATION_JSON_VALUE})
+    public String getRequestParam(@RequestParam(required = false, name = "first") String firstParam) {
+        System.out.println("First param: " + firstParam);
+        return firstParam;
+    }
+
+    @GetMapping("/pathVar/{pathVar1}")
+    public String getPathVariable(@PathVariable(name = "pathVar1") String pathVar1) {
+        return pathVar1;
+    }
+
+    @GetMapping("/rsEntity")
+    public ResponseEntity<?> getEntity() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+            .header("header-test", "test")
+            .build();
+    }
+
+    @GetMapping("/header")
+    public ResponseEntity<?> getHeader(@RequestHeader(value = "hello", defaultValue = "12") Integer helloHeader) {
+        return ResponseEntity.ok()
+            .header("header-test", helloHeader.toString())
+            .build();
+    }
+
+    @GetMapping("/message")
+    @PreDestroy
+    public Message getMessage() {
+        Message body = new Message();
+        body.setValue("message");
+        return body;
+    }
+
+    @PostMapping
+    public Message createMessage(@RequestBody(required = false) Message message) {
+        if (message == null || message.getValue() == null) {
+            throw new MessageNotFoundException("message is null");
+        }
+        message = service.doHandleMessage(message);
+        return message;
+    }
+
+    @ExceptionHandler({MessageNotFoundException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Error handleNotFound(MessageNotFoundException ex) {
+        return new Error(ex.getMessage());
+    }
+}
