bd9261686ab17c65604b6c276284ffc00051bfbb
rassafel <rassafel@gmail.com>
2022-11-15 19:58:54 +0400

    Add message repository implementation.

---
+++ spring-learn/src/main/java/ru/nshi/repository/MessageRepositoryImpl.java
@@ -0,0 +1,59 @@
+package ru.nshi.repository;
+
+import org.springframework.stereotype.Repository;
+import ru.nshi.error.MessageNotFoundException;
+import ru.nshi.model.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Repository
+public class MessageRepositoryImpl implements MessageRepository {
+    private final Map<Integer, Message> data = new ConcurrentHashMap<>();
+    private final AtomicInteger autoId = new AtomicInteger(1);
+
+    @Override
+    public List<Message> findAll() {
+        return new ArrayList<>(data.values());
+    }
+
+    @Override
+    public Message getById(Integer id) {
+        Message result = data.get(id);
+        if (result == null) {
+            throw new MessageNotFoundException("message not found");
+        }
+        return result;
+    }
+
+    @Override
+    public Message save(Message message) {
+        int id = autoId.incrementAndGet();
+        message.setId(id);
+        data.put(id, message);
+        return message;
+    }
+
+    @Override
+    public Message updateById(Integer id, Message message) {
+        Message oldValue = getById(id);
+        if (oldValue == null) {
+            throw new MessageNotFoundException("message not found");
+        }
+        message.setId(id);
+        data.put(id, message);
+        return message;
+    }
+
+    @Override
+    public Message deleteById(Integer id) {
+        Message result = data.remove(id);
+        if (result == null) {
+            throw new MessageNotFoundException("message not found");
+        }
+        return result;
+    }
+}
